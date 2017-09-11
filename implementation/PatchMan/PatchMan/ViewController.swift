f81b1266881b4a4177895d7b6a3c176bed3b6122
sanuki <applerito@gmail.com>
2017-09-11 10:41:36 +1000

    Deleted template and restarted TableView without CoreData implementation

---
+++ implementation/PatchMan/PatchMan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PatchMan
+//
+//  Created by sanuki on 11/9/17.
+//  Copyright © 2017 James Greatbanks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    
+}
+
--- implementation/acknowledgements.md
+++
@@ -1,7 +0,0 @@
-# Sources/Acknowledgements
----
-
-Template Xcode project (TableView and CoreData) sourced from:
-
-[https://www.raywenderlich.com/145809/getting-started-core-data-tutorial](https://www.raywenderlich.com/145809/getting-started-core-data-tutorial)
-
